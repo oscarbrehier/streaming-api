b22ff2500365e0565e9580ddc6f4721f01f2670f
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2025-11-27 11:02:08 +0100

    added app url to cors

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,19 @@ import routes from "./routes/index.js";
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({
-	origin: "http://localhost:3000"
-}));
+const allowedDomains = [
+	"http://localhost:3000",
+	process.env.APP_URL
+].filter(d => d != undefined);
 
-app.use("/api", routes);
+app.use(cors({
+	origin: allowedDomains
+}));
 
 app.use(express.json({ limit: "5gb" }));
 app.use(express.urlencoded({ limit: "5gb", extended: true }));
+
+app.use("/api", routes);
 
 app.listen(port, () => {
 	console.log(`Listening on port: ${port}`);
