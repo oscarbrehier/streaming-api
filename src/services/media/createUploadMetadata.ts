77f868811d8bbff1204ff199a27be7ddad193537
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2026-01-14 10:39:51 +0100

    chore: remove deprecated routes /media, /streaming, /watch

--- src/services/media/createUploadMetadata.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-export async function createUploadMetadata(metadataPath: string, data: Record<string, any>) {
-
-	const jsonData = JSON.stringify(data, null, 2);
-	await fs.promises.writeFile(metadataPath, jsonData, "utf-8");
-
-};
