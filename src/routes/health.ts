aa774c543da70f7fb594b52ff0dbf97808ad36bc
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2026-01-29 15:56:22 +0100

    simple no auth health route

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,9 +1,12 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { sysInfoController } from "../controllers/health/system.controllers.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
+router.get("/", (req: Request, res: Response) => {
+	return res.sendStatus(200);
+});
 router.get("/system", authMiddleware, sysInfoController);
 
 export default router;
